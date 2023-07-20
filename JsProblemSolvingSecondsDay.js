7486547065d1dac2c6296fc84901e7f5419eea0f
Sakhawat Hosen <sakhawathosen320@gmail.com>
2023-07-21 01:19:19 +0600

    anther problem solving

---
+++ JsProblemSolvingSecondsDay.js
@@ -0,0 +1,35 @@
+// const str = "I love Bangladesh";
+
+// // output = "hdgkfsdhfgjkahsd"
+
+// function reverseWords(data){
+//     const dataAsArray = data.split(" ");
+//     const result = dataAsArray.map((d)=>{
+//         return d.split("").reverse().join("");
+
+//     })
+
+//     return result.join(" ");
+
+
+// }
+
+
+// console.log(reverseWords(str));
+
+const string =" Java Is a Programming Language"
+
+function shortWords (data){
+    const dataAsArrays= string.split(" ");
+
+    dataAsArrays.sort((a,b) =>{
+        return a.length - b.length;
+
+    });
+    return dataAsArrays.join(" ");
+
+}
+
+
+console.log(shortWords(string))
+
